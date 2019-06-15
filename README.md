9d2c29286ff72a0d407644059432c781e45ddd05
xiexianbin <me@xiexianbin.cn>
2019-06-15 22:53:09 +0800

    feature: init func

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
+hugo-algolia
+--
 
+a tools sync hugo json index to algolia.
+
+## how to build
+
+```
+go build hugo-algolia.go
+```
+
+## how to config
+
+create a `algolia.yaml` file, like:
+
+```
+---
+app_id: "<app id>"
+admin_key: "<admin key>"
+index_name: "<index name>"
+index_file: "./public/algolia.json"
+```
+
+note:
+
+- app_id: app id
+- admin_key: admin key
+- index_name: index name
+- index_file: json file path
+
+
+## how to use
+
+```
+./hugo-algolia algolia.yaml
+```
---
+++ hugo-algolia.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/algolia/algoliasearch-client-go/algoliasearch"
+)
+
+type Config struct {
+	AppId     string `yaml:"app_id"`
+	AdminKey  string `yaml:"admin_key"`
+	IndexName string `yaml:"index_name"`
+	IndexFile string `yaml:"index_file"`
+}
+
+func Sync2Algolia() {
+	client := algoliasearch.NewClient(cfg.AppId, cfg.AdminKey)
+	index := client.InitIndex(cfg.IndexName)
+
+	content, _ := ioutil.ReadFile(cfg.IndexFile)
+
+	var objects []algoliasearch.Object
+	if err := json.Unmarshal(content, &objects); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	res, err := index.AddObjects(objects)
+	if err != nil {
+		fmt.Println("Sync to algolia failed: ", err)
+	} else {
+		fmt.Println("Task id: ", res.TaskID)
+		fmt.Println("Object IDs: ", res.ObjectIDs)
+		fmt.Println("Sync to algolia Success.")
+	}
+}
+
+var cfg Config
+
+func main() {
+	if len(os.Args) < 2 {
+		println("Usage: ./hugo-algolia <ConfigFile>\n")
+		return
+	}
+
+	_cfg, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Println("Read config file failed: ", err)
+		return
+	}
+
+	err = json.Unmarshal(_cfg, &cfg)
+	if err != nil {
+		log.Println("Unmarshal config failed: ", err)
+		return
+	} else {
+		Sync2Algolia()
+	}
+}
